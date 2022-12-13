14fe56ba5b52d1df538f1f70dbf57a8c779eb219
HomeNote <alexzep@yandex.ru>
2022-12-13 12:50:35 +0100

    lvl4 code optimisation Added .map function

--- __tests__/differences-generator.test.js
+++ __tests__/differences-generator.test.js
@@ -7,9 +7,16 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-test('differencesGenerator', () => {
+test('differencesGenerator_JSON', () => {
   const expected = '- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: true';
   const path1 = getFixturePath('file1.json');
   const path2 = getFixturePath('file2.json');
+  expect(differencesGenerator(path1, path2)).toEqual(expected);
+});
+
+test('differencesGenerator_YAML', () => {
+  const expected = '- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: true';
+  const path1 = getFixturePath('file1.yml');
+  const path2 = getFixturePath('file2.yaml');
   expect(differencesGenerator(path1, path2)).toEqual(expected);
 });
